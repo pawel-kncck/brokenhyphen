6bc41d5cbd418e46a9ad406cc026183501f73229
Pawel Kuncicki <pkuncicki@Pawels-MacBook-Pro.local>
2020-10-02 10:46:57 +0200

    onChange in text editor

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const testOutput = document.getElementById('test-output')
+const editor = document.getElementById('editor')
+let editorValue = document.getElementById('editor').innerText
+
+const getElementValue = (domPath, elementId) => {
+    let result = null;
+    domPath.forEach(element => {
+        if (element.id === elementId) result = element.innerText
+    });
+    return result;
+}
+
+
 const init = () => {
     console.log("App initialized")
+    
+    editor.addEventListener('DOMSubtreeModified', (event) => {console.log(getElementValue(event.path,'editor'))})
 
 }
 
