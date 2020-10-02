2eb35e3b9cb162459ccbf7eaa0a5d5809e4d37c9
Pawel Kuncicki <pkuncicki@Pawels-MacBook-Pro.local>
2020-10-02 12:22:27 +0200

    selection detection, adding startIndex and endIndex to span

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const testOutput = document.getElementById('test-output')
 const editor = document.getElementById('editor')
-editor.innerHTML = '<span>test</span>'
+editor.innerHTML = '<span>123456789</span><span>123456789</span><span>123456789</span>'
 let editorValue = document.getElementById('editor').innerText
 
 const getElementValue = (domPath, elementId) => {
@@ -15,13 +15,42 @@ const getElementValue = (domPath, elementId) => {
 const init = () => {
     console.log("App initialized")
 
+    const indexSpans = () => {
+        const spanNodes = document.getElementsByTagName("span")
+        const spanArray = Array.prototype.slice.call(spanNodes)
+        let i = 0;
+        spanArray.forEach(element => {
+            const len = element.innerText.length
+
+            element.setAttribute("startIndex", i + 1);
+            element.setAttribute("endIndex", i + len);
+            i = i + len;
+        });
+    }
+
+    indexSpans();
+
     const printSelection = () => {
-        if (window.getSelection().anchorOffset !== window.getSelection().extentOffset) {
+        const anchorStartIndex = window.getSelection().anchorNode.parentElement.attributes.startindex.value;
+        const anchorEndIndex = window.getSelection().anchorNode.parentElement.attributes.endindex.value;
+        const extentStartIndex = window.getSelection().extentNode.parentElement.attributes.startindex.value;
+        const extentEndIndex = window.getSelection().extentNode.parentElement.attributes.endindex.value;
+
+        const selectionStartIndex = window.getSelection().anchorOffset + parseInt(anchorStartIndex)
+        const selectionEndIndex = window.getSelection().extentOffset + parseInt(extentStartIndex)
+
+        if (selectionStartIndex !== selectionEndIndex) {
+
+            console.log(selectionStartIndex);
+            console.log(selectionEndIndex);
+
             console.log(window.getSelection())
+
         }
     }
     
-    // editor.addEventListener('DOMSubtreeModified', (event) => {console.log(window.getSelection())})
+    editor.addEventListener('DOMSubtreeModified', indexSpans)
+
     editor.addEventListener('mouseup', () => printSelection())
     editor.addEventListener('keyup', () => printSelection())
 
