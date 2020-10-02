d17c74729f166b0fc79917201bd49224c95e4781
Pawel Kuncicki <pkuncicki@Pawels-MacBook-Pro.local>
2020-10-02 11:21:39 +0200

    adding get selection

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const testOutput = document.getElementById('test-output')
 const editor = document.getElementById('editor')
+editor.innerHTML = '<span>test</span>'
 let editorValue = document.getElementById('editor').innerText
 
 const getElementValue = (domPath, elementId) => {
     let result = null;
     domPath.forEach(element => {
-        if (element.id === elementId) result = element.innerText
+        if (element.id === elementId) result = element.innerHTML
     });
     return result;
 }
@@ -13,8 +14,16 @@ const getElementValue = (domPath, elementId) => {
 
 const init = () => {
     console.log("App initialized")
+
+    const printSelection = () => {
+        if (window.getSelection().anchorOffset !== window.getSelection().extentOffset) {
+            console.log(window.getSelection())
+        }
+    }
     
-    editor.addEventListener('DOMSubtreeModified', (event) => {console.log(getElementValue(event.path,'editor'))})
+    // editor.addEventListener('DOMSubtreeModified', (event) => {console.log(window.getSelection())})
+    editor.addEventListener('mouseup', () => printSelection())
+    editor.addEventListener('keyup', () => printSelection())
 
 }
 
